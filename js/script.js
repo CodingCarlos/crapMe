905d184990089e7c3f42e095ba51cb9a6b18dcee
Carlos Hernandez <carloshdmn@gmail.com>
2016-03-01 22:46:36 +0100

    Main app

---
+++ js/script.js
@@ -0,0 +1,53 @@
+var iniMinutes = 0;
+var counter = 0;
+var interval;
+var element;
+
+/* Controls */
+ 
+// Start
+$('#takePhoto').on('click', function() {
+	// TAKE PHOTO
+	takePhoto();
+});
+
+/* Functions */
+
+// Resize
+function resize(image) {
+
+	var imageDataInBase64 = 
+
+	window.imageResizer.resizeImage(
+	function(data) { 
+		var image = document.getElementById('myImage');
+		image.src = "data:image/jpeg;base64," + data.imageData; 
+	}, function (error) {
+		console.log("Error : \r\n" + error);
+	}, imageDataInBase64, 0.5, 0.5, {
+		resizeType: ImageResizer.RESIZE_TYPE_FACTOR,
+		format: 'jpg'
+	});
+}
+
+
+function takePhoto(){
+	navigator.camera.getPicture(onPhotoDataSuccess, onFail, { 
+		quality: 20, 
+		allowEdit: true, 
+		destinationType: navigator.camera.DestinationType.DATA_URL 
+	});
+}
+
+function onPhotoDataSuccess(imageData) {
+	var photo = document.getElementById('photo');
+	photo.style.display = 'block';
+	photo.src = "data:image/jpeg;base64," + imageData;
+
+	// var sendPhoto = document.getElementById('sendPhoto');
+	// sendPhoto.style.display = 'block';
+}
+
+function onFail(message) {
+	alert('Failed because: ' + message);
+}
